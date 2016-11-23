3bf1b8ebf38a8ba1991f0d6e30b05a1ff9b41ead
Burke Libbey <burke@libbey.me>
2016-11-22 21:25:18 -0500

    treeview for methods

--- frontend/js/components/ClassDetail.js
+++ frontend/js/components/ClassDetail.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import Relay from 'react-relay';
+import TreeView from 'react-treeview';
 
 class ClassDetail extends React.Component {
   render() {
     var klass = this.props.store;
     if (klass) {
       return (
-        <div className="pane-content">
+        <div className="class-detail pane-content">
           <h1 className="inset">{klass.name}</h1>
           <hr />
           {klass.ancestors.map(anc => (
             <div key={anc.id}>
-              <h2 className="ancestor inset">Defined on <span className={"module"}>{anc.name}</span></h2>
-              <ul className="methods inset">
-                {anc.methods.map(method => (
-                  <li key={method.id} className={method.visibility}>
-                    <a onClick={() => this.props.controller.setFocusMethod(method)} href="#">
-                      {method.name}
-                    </a>
-                  </li>
-                ))}
-              </ul>
+              <TreeView
+                nodeLabel={
+                  <span>Defined on <span className={"module"}>{anc.name}</span></span>
+                }
+                defaultCollapsed={false}
+              >
+                <ul className="methods inset">
+                  {anc.methods.map(method => (
+                    <li key={method.id} className={method.visibility}>
+                      <a onClick={() => this.props.controller.setFocusMethod(method)} href="#">
+                        {method.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </TreeView>
             </div>
           ))}
         </div>
