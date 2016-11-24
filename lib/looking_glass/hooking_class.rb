841d6483be0fc5d1e06edb0b81383efe6ada62cf
Burke Libbey <burke@libbey.me>
2016-11-24 17:23:00 -0500

    apparently overriding inspect is a thing

--- lib/looking_glass/hooking_class.rb
+++ lib/looking_glass/hooking_class.rb
@@ -1,5 +1,6 @@
 module LookingGlass
   CLASS_DEFINITION_POINTS = {}
+  MODULE_INSPECT = Module.method(:inspect).unbind
 
   module HookingClass
     class << self
@@ -11,7 +12,8 @@ module LookingGlass
           alias_method :old_inherited, :inherited
           def inherited(subclass)
             file = caller[0].sub(/:\d+:in.*/, '')
-            LookingGlass::CLASS_DEFINITION_POINTS[subclass.inspect] = file
+            key = MODULE_INSPECT.bind(subclass).call
+            LookingGlass::CLASS_DEFINITION_POINTS[key] = file
             old_inherited(subclass)
           end
         end
